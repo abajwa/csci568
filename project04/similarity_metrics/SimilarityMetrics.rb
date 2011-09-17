0daca35e5f3bf776ae1ce9b3813cf0a3d9aa28a2
Amanreet Bajwa <abajwa@mines.edu>
2011-09-16 23:16:43 -0600

    Added SCM and its test

--- project04/similarity_metrics/SimilarityMetrics.rb
+++ project04/similarity_metrics/SimilarityMetrics.rb
@@ -1,11 +1,21 @@
-# Euclidean distance function.
+# Assuming (for all functions) both of the objects are of the same type with the same attributes
+
+# Euclidean Distance function
 def euclideanDistance(obj1, obj2)
 	running_total = 0
-	# Assuming both of the objects are of the same type with the same attributes
 	for obVar in obj1.instance_variables
 		running_total += (obj1.instance_variable_get(obVar) - obj2.instance_variable_get(obVar))**2
 	end
 	Math.sqrt(running_total)
 end
 
-
+# Simple Matching Coefficients (SMC) function
+def smc(obj1, obj2)
+	similar = 0;
+	for obVar in obj1.instance_variables
+		if obj1.instance_variable_get(obVar) == obj2.instance_variable_get(obVar)
+			similar += 1
+		end
+	end
+	similar/obj1.instance_variables.length
+end
--- project04/similarity_metrics/tests.rb
+++ project04/similarity_metrics/tests.rb
@@ -12,10 +12,20 @@ end
 
 object1 = TestClass.new(1,3,4,6)
 object2 = TestClass.new(1,4,5,9)
+object3 = TestClass.new(8,7,5,10)
+object4 = TestClass.new(4,5,3,5)
 
 print "Euclidean Distance test..."
 euclideanAnswer = Math.sqrt(11)
 if euclideanAnswer ==  euclideanDistance(object1, object2)
+	puts "PASS"
+else
+	puts "FAIL"
+end
+
+print "SMC test..."
+smcAnswer = 1/4
+if(smcAnswer == smc(object1, object2))
 	puts "PASS"
 else
 	puts "FAIL"

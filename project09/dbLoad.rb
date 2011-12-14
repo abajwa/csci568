0d14a0784d8ca0536cd3e8b190225aa2d0fc61f4
Amanreet Bajwa <abajwa@mines.edu>
2011-12-13 17:18:19 -0700

    added users/ratings to the database and cleaned up the dbLoad program

--- project09/dbLoad.rb
+++ project09/dbLoad.rb
@@ -51,43 +51,44 @@ Genre.delete_all
 Album.delete_all
 Artist.delete_all
 Track.delete_all
-
-#puts "Loading genres..."
-## loads genres into the database
-#File.open("track1/genreData1.txt").each do |record|
-#	Genre.create(:id => record)
-#end
-#
-#puts "Loading artists..."
-## loads artists into the database
-#File.open("track1/artistData1.txt").each do |record|
-#	Artist.create(:id => record)
-#end
-#
-#puts "Loading albums..."
-## loads albums into the database
-#File.open("track1/albumData1.txt").each do |line|
-#	tokens = line.split("|")
-#	genres = tokens[2..tokens.length]
-#
-#	genres.map! {|g| Genre.find(g)}
-#	Album.create(:id => tokens[0], :artist_id => tokens[1], :genres => genres)
-#end
-#
-#puts "Loading tracks..."
-## loads tracks into the database
-#File.open("track1/trackData1.txt").each do |line|
-#	tokens = line.split("|")
-#	genres = tokens[3..tokens.length]
-#
-#	genres.map! {|g| Genre.find(g)}
-#	Track.create(:id => tokens[0], :album_id => tokens[1], :artist_id => tokens[2], :genres => genres)
-#end
-
-# Adds users and their ratings to the database
-userId = 0
 User.delete_all
 Rating.delete_all
+
+puts "Loading genres..."
+# loads genres into the database
+File.open("track1/genreData1.txt").each do |record|
+	Genre.create(:id => record)
+end
+
+puts "Loading artists..."
+# loads artists into the database
+File.open("track1/artistData1.txt").each do |record|
+	Artist.create(:id => record)
+end
+
+puts "Loading albums..."
+# loads albums into the database
+File.open("track1/albumData1.txt").each do |line|
+	tokens = line.split("|")
+	genres = tokens[2..tokens.length]
+
+	genres.map! {|g| Genre.find(g)}
+	Album.create(:id => tokens[0], :artist_id => tokens[1], :genres => genres)
+end
+
+puts "Loading tracks..."
+# loads tracks into the database
+File.open("track1/trackData1.txt").each do |line|
+	tokens = line.split("|")
+	genres = tokens[3..tokens.length]
+
+	genres.map! {|g| Genre.find(g)}
+	Track.create(:id => tokens[0], :album_id => tokens[1], :artist_id => tokens[2], :genres => genres)
+end
+
+puts "Loading users and ratings..."
+# Adds users and their ratings to the database
+userId = 0
 File.open("track1/trainIdx1.firstLines.txt").each do |line|
 	tokens = line.split("|")
 	if tokens.length == 2
@@ -99,5 +100,3 @@ File.open("track1/trainIdx1.firstLines.txt").each do |line|
 		Rating.create(:user_id => userId, :rating => tokens[1], :item_id => tokens[0])
 	end
 end
-
-
